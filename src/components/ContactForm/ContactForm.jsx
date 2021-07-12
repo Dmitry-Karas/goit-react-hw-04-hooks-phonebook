7b668992bde2136f2c3f1d19733725465ec8b18d
Dmitry Karas <dmitrythekaras@gmail.com>
2021-07-12 16:34:29 +0300

    all done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { nanoid } from "nanoid";
-import { Component } from "react";
+import { useState } from "react";
 import {
   StyledForm,
   InputContainer,
@@ -10,84 +10,168 @@ import {
 } from "./ContactForm.styled";
 import { FiUser, FiPhone, FiUserPlus } from "react-icons/fi";
 
-export class ContactForm extends Component {
-  state = { contacts: [], name: "", number: "" };
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  nameInputId = nanoid();
-  telInputId = nanoid();
-  contactId = nanoid();
+  const nameInputId = nanoid();
+  const telInputId = nanoid();
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.currentTarget;
 
-    this.setState({ [name]: value });
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+
+      case "number":
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    this.props.onSubmit(this.state);
-    this.resetForm();
+    onSubmit({ name, number });
+
+    setName("");
+    setNumber("");
   };
 
-  resetForm = () => {
-    this.setState({ name: "", number: "" });
-  };
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <InputContainer>
+        <Input
+          id={nameInputId}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          autoComplete="off"
+          placeholder="Dmitry Karas"
+          maxLength="40"
+          required
+        />
 
-  render() {
-    const { name, number } = this.state;
+        <Label htmlFor={nameInputId}>
+          <FiUser size="22" />
+          Name
+        </Label>
+      </InputContainer>
 
-    return (
-      <StyledForm onSubmit={this.handleSubmit}>
-        <InputContainer>
-          <Input
-            id={this.nameInputId}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            autoComplete="off"
-            placeholder="Dmitry Karas"
-            maxLength="40"
-            required
-          />
+      <InputContainer>
+        <Input
+          id={telInputId}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять из цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          autoComplete="off"
+          placeholder="444-55-66"
+          maxLength="17"
+          required
+        />
+        <Label htmlFor={telInputId}>
+          <FiPhone size="22" />
+          Number
+        </Label>
+      </InputContainer>
 
-          <Label htmlFor={this.nameInputId}>
-            <FiUser size="22" />
-            Name
-          </Label>
-        </InputContainer>
+      <Button type="submit">
+        <FiUserPlus size="30" />
+        add
+      </Button>
+    </StyledForm>
+  );
+};
 
-        <InputContainer>
-          <Input
-            id={this.telInputId}
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять из цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            autoComplete="off"
-            placeholder="444-55-66"
-            maxLength="17"
-            required
-          />
-          <Label htmlFor={this.telInputId}>
-            <FiPhone size="22" />
-            Number
-          </Label>
-        </InputContainer>
+// export class ContactForm extends Component {
+//   state = { contacts: [], name: "", number: "" };
 
-        <Button type="submit">
-          <FiUserPlus size="30" />
-          add
-        </Button>
-      </StyledForm>
-    );
-  }
-}
+//   nameInputId = nanoid();
+//   telInputId = nanoid();
+//   contactId = nanoid();
+
+//   handleChange = (e) => {
+//     const { name, value } = e.currentTarget;
+
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     this.props.onSubmit(this.state);
+//     this.resetForm();
+//   };
+
+//   resetForm = () => {
+//     this.setState({ name: "", number: "" });
+//   };
+
+//   render() {
+//     const { name, number } = this.state;
+
+// return (
+//   <StyledForm onSubmit={this.handleSubmit}>
+//     <InputContainer>
+//       <Input
+//         id={this.nameInputId}
+//         type="text"
+//         name="name"
+//         value={name}
+//         onChange={this.handleChange}
+//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//         title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//         autoComplete="off"
+//         placeholder="Dmitry Karas"
+//         maxLength="40"
+//         required
+//       />
+
+//       <Label htmlFor={this.nameInputId}>
+//         <FiUser size="22" />
+//         Name
+//       </Label>
+//     </InputContainer>
+
+//     <InputContainer>
+//       <Input
+//         id={this.telInputId}
+//         type="tel"
+//         name="number"
+//         value={number}
+//         onChange={this.handleChange}
+//         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//         title="Номер телефона должен состоять из цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+//         autoComplete="off"
+//         placeholder="444-55-66"
+//         maxLength="17"
+//         required
+//       />
+//       <Label htmlFor={this.telInputId}>
+//         <FiPhone size="22" />
+//         Number
+//       </Label>
+//     </InputContainer>
+
+//     <Button type="submit">
+//       <FiUserPlus size="30" />
+//       add
+//     </Button>
+//   </StyledForm>
+// );
+//   }
+// }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,45 +1,76 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import { AiOutlineFileSearch } from "react-icons/ai";
 import { InputContainer, Input, Label } from "./Filter.styled";
 
-export class Filter extends Component {
-  state = {
-    value: "",
-  };
+export const Filter = ({ onChange }) => {
+  const [value, setValue] = useState("");
 
-  filterInputId = nanoid();
+  const filterInputId = nanoid();
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
 
-    this.setState({ value });
-    this.props.onChange(value);
+    setValue(value);
+    onChange(value);
   };
 
-  render() {
-    const { value } = this.state;
+  return (
+    <>
+      <InputContainer>
+        <Input
+          id={filterInputId}
+          type="text"
+          value={value}
+          onChange={handleChange}
+          placeholder="Dmitry"
+        />
+        <Label htmlFor={filterInputId}>
+          <AiOutlineFileSearch size="30" />
+          Find contacts by name
+        </Label>
+      </InputContainer>
+    </>
+  );
+};
 
-    return (
-      <>
-        <InputContainer>
-          <Input
-            id={this.filterInputId}
-            type="text"
-            value={value}
-            onChange={this.handleChange}
-            placeholder="Dmitry"
-          />
-          <Label htmlFor={this.filterInputId}>
-            <AiOutlineFileSearch size="30" />
-            Find contacts by name
-          </Label>
-        </InputContainer>
-      </>
-    );
-  }
-}
+// export class Filter extends Component {
+//   state = {
+//     value: '',
+//   }
+
+//   filterInputId = nanoid()
+
+//   handleChange = (e) => {
+//     const { value } = e.target
+
+//     this.setState({ value })
+//     this.props.onChange(value)
+//   }
+
+//   render() {
+//     const { value } = this.state
+
+//     return (
+//       <>
+//         <InputContainer>
+//           <Input
+//             id={this.filterInputId}
+//             type="text"
+//             value={value}
+//             onChange={this.handleChange}
+//             placeholder="Dmitry"
+//           />
+//           <Label htmlFor={this.filterInputId}>
+//             <AiOutlineFileSearch size="30" />
+//             Find contacts by name
+//           </Label>
+//         </InputContainer>
+//       </>
+//     )
+//   }
+// }
 
 Input.propTypes = {
   value: PropTypes.string.isRequired,
